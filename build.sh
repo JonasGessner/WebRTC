d8017937fe000ccdddc7b7c6f212c950d4ac0d48
Jonas Gessner <JonasGessner@users.noreply.github.com>
2021-04-08 14:47:36 +0200

    Add arm macOS build

--- build.sh
+++ build.sh
@@ -22,11 +22,17 @@ gn gen out/ios_x64 --args='target_os="ios" target_cpu="x64" use_xcode_clang=true
 echo Building simulator target
 ninja -C out/ios_x64 framework_objc
 
-echo Making macOS target
+echo Making macOS-x86 target
 gn gen out/mac_x64 --args='target_os="mac" target_cpu="x64" is_component_build=false rtc_include_tests=false use_goma=false rtc_enable_symbol_export=true '"$debug"' rtc_libvpx_build_vp9=true rtc_use_metal_rendering=true mac_deployment_target="10.11" rtc_enable_protobuf=true'
 
 echo Building macOS target
 ninja -C out/mac_x64 mac_framework_objc
+
+echo Making macOS-ARM target
+gn gen out/mac_arm --args='target_os="mac" target_cpu="arm64" is_component_build=false rtc_include_tests=false use_goma=false rtc_enable_symbol_export=true '"$debug"' rtc_libvpx_build_vp9=true rtc_use_metal_rendering=true mac_deployment_target="10.11" rtc_enable_protobuf=true'
+
+echo Building macOS target
+ninja -C out/mac_arm mac_framework_objc
 
 echo "Sadly catalyst builds don't work."
 # A catalyst build would look like this:
@@ -35,4 +41,4 @@ echo "Sadly catalyst builds don't work."
 
 echo Making xcframework
 rm -rf ./out/_WebRTC.xcframework
-xcodebuild -create-xcframework -framework ./out/ios_x64/WebRTC.framework -framework ./out/ios_arm64/WebRTC.framework -framework ./out/mac_x64/WebRTC.framework -output ./out/_WebRTC.xcframework && rm -rf ./out/WebRTC.xcframework && mv ./out/_WebRTC.xcframework ./out/WebRTC.xcframework
+xcodebuild -create-xcframework -framework ./out/ios_x64/WebRTC.framework -framework ./out/ios_arm64/WebRTC.framework -framework ./out/mac_x64/WebRTC.framework -framework ./out/mac_arm/WebRTC.framework -output ./out/_WebRTC.xcframework && rm -rf ./out/WebRTC.xcframework && mv ./out/_WebRTC.xcframework ./out/WebRTC.xcframework
